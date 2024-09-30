485ecd36b362eabad0b727fdd8b4c0c1624aee5f
JeongMin <jmgold96@gmail.com>
2024-09-30 16:33:16 +0900

    제곱함수

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ def add_func(n1,n2):
 def sub_func(n1,n2):
     return n1-n2
 
+def sqr_func(n1,n2):
+    return n1**2,n2**2
+
 ## 전역 변수부
 num1, num2, res = 100, 200, 0
 
@@ -14,3 +17,7 @@ print(num1,'+',num2,'=',res)
 
 res = sub_func(num1, num2)
 print(num1,'-',num2,'=',res)
+
+res = sqr_func(num1, num2)
+print(num1,num2,'=>',res)
+
